586baae3a2dc778c124b6d29e7146ae126b11771
Sugangs <135301151+Sugangs@users.noreply.github.com>
2023-06-02 10:32:42 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+
+public class HelloWorld {
+                    public void myName() {
+                    	
+                    	
+                    }
+                    public void myMethod1() { 	
+                    	System.out.println(" myMethod1 "); 	
+                    	} 
+                    
+                    public void myMethod2() { 	
+                    	System.out.println(" myMethod2 ");
+                    	} 
+                    
+                    public void mymethod3() { 	
+                    	System.out.println(" mymethod3 "); 
+                    	} 	 	
+                   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello World !!!!!");
+		System.out.println(" Hello Java !!!!!");
+		HelloWorld ob = new HelloWorld();
+		ob.myMethod1();
+		ob.myMethod2();
+		ob.mymethod3();
+	}
+
+}
